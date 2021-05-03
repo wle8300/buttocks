614a82d08fd2a52b63ca226c8e0f5839d6744a1b
William Le <williamle@Jennifers-MBP.lan>
2021-05-03 15:14:23 -0700

    asdf

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        WHJTATUPTPTUP
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
